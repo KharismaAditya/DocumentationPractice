74847b67176b46d31b4b57e66e6e7c3d22b55e50
KharismaAditya <yenderruaditya@gmail.com>
2025-10-20 09:36:50 +0700

    Kalkulator Penjumalahan

---
+++ Calculator.java
@@ -0,0 +1,11 @@
+class Calculator{
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int num1, int num2, int result;
+        System.out.println("==KALKULATOR PENJUMLAHAN SEDERHANA==");
+        System.out.print("Masukkan angka 1: ");num1 = input.nextInt;
+        System.out.print("Masukkan angka 2: ");num2 = input.nextInt;
+        result = num1 + num2;
+        System.out.println("Hasil perjumlahan: " + result);
+    }
+}
