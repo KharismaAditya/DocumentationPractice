912f0ef00ed042c68117a800607789240f856e86
KharismaAditya <yenderruaditya@gmail.com>
2025-10-20 09:40:40 +0700

    v2

--- Calculator.java
+++ Calculator.java
@@ -2,6 +2,8 @@ class Calculator{
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int num1, int num2, int result;
+        System.out.println("Muhammad Khaspdksajkfbsdlbfklsdfnsdl;af");
+        System.out.println("sajdbsjabdsajdlksadsad");
         System.out.println("==KALKULATOR PENJUMLAHAN SEDERHANA==");
         System.out.print("Masukkan angka 1: ");num1 = input.nextInt;
         System.out.print("Masukkan angka 2: ");num2 = input.nextInt;
