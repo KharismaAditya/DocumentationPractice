3378c10cc5fa357bdf177991295655185c4ea4c5
KharismaAditya <yenderruaditya@gmail.com>
2025-10-20 09:37:34 +0700

    Kalkulator Pengurangan

--- Calculator.java
+++ Calculator.java
@@ -2,10 +2,10 @@ class Calculator{
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int num1, int num2, int result;
-        System.out.println("==KALKULATOR PENJUMLAHAN SEDERHANA==");
+        System.out.println("==KALKULATOR PENGURANGAN SEDERHANA==");
         System.out.print("Masukkan angka 1: ");num1 = input.nextInt;
         System.out.print("Masukkan angka 2: ");num2 = input.nextInt;
-        result = num1 + num2;
+        result = num1 - num2;
         System.out.println("Hasil perjumlahan: " + result);
     }
 }
